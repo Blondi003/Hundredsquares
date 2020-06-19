838fd3f4b3bce621e5c09f2090971deb5e4d3634
Blondi003 <59411510+Blondi003@users.noreply.github.com>
2020-06-19 14:02:47 +0200

    added array.map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import './index.js';
 
-function App() {
-  return <div></div>;
-}
+//var arr = [square];
+
+//for (var i = 0; i <= 100; i++) {
+//arr.push(i);
+//}
+
+var arr = [square];
+var hundred = arr.map(function (x) {
+  return x * 100;
+});
 
 export default App;
