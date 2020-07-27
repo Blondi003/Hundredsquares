eaa917451798edb0cd0b02a4d9190343225500e9
Blondi003 <59411510+Blondi003@users.noreply.github.com>
2020-07-27 16:03:40 +0200

    hundred black squares

--- src/Squares/index.js
+++ src/Squares/index.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import './styles.css';
 
-const getColor = () => {
-  let color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
-  while (color.length < 6) {
-    color.click = '0' + color;
-  }
-
-  return '#' + color;
-};
-
 function Squares() {
   const anArray = new Array(100).fill();
-
-  console.log(anArray);
 
   return (
     <ol>
       {anArray.map((item, index) => {
-        const color = getColor();
-
-        return (
-          <div
-            className='square'
-            key={index}
-            style={{ backgroundColor: color }}
-          />
-        );
+        return <div className='square' key={index} />;
       })}
     </ol>
   );
